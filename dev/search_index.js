cefdc35bcd5325b9a6db71949619c437fb5e1332
Documenter.jl <documenter@juliadocs.github.io>
2021-09-08 12:51:04 +0000

    build based on 2d3f340

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PERMANOVA","category":"page"},{"location":"#PERMANOVA","page":"Home","title":"PERMANOVA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PERMANOVA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PERMANOVA]","category":"page"}]
+}
