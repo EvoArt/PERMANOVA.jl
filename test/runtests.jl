c3bcf94fce9f64ea839210ec7436ef0111224677
Arthur <arn203@exeter.ac.uk>
2021-09-08 21:56:45 +0100

    nperm range

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,7 @@ preds3 = [rand(["a","b","c","d"]) for i in x]
 df = DataFrame([preds,preds2,preds3],[:X,:Z,:W])
 Hyde = hydra(df,y,BrayCurtis,@formula(1~X+Z+W),100)
 R"adonis2($y ~X+Z+W,$df,1000)"
-for nperm in [1000:1000:20000]
+for nperm in 1000:1000:20000
     J = @benchmark permanova(df,y,BrayCurtis,@formula(1~X+Z+W),nperm)
     R = @benchmark R"adonis2($y ~X+Z+W,$df,nperm)"
 end
