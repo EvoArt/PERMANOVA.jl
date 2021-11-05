6b7a91a31ab171bdb2380db3f609e35ede275884
Arthur <arn203@exeter.ac.uk>
2021-11-05 10:00:05 +0000

    test larger input

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,3 +19,21 @@ n_terms = 3
 @test sum(R"ad$SumOfSqs".≈ Hyde.results[:,"SumOfSqs"]) == n_terms +2
 @test sum(R"ad$R2".≈ Hyde.results[:,"R2"]) == n_terms +2
 @test sum(R"ad$F".≈ Hyde.results[:,"F"]) == n_terms
+
+
+x = rand(1:4,1000)
+z = rand(1:4,1000)
+y = rand(1000,5) .+ (x .* (x .==3) .*(z .>2))
+
+preds = [["a","b","c","d"][i] for i in x]
+preds2 = [["a","b","c","d"][i] for i in z]
+preds3 = [rand(["a","b","c","d"]) for i in x]
+df = DataFrame([preds,preds2,preds3],[:X,:Z,:W])
+Hyde = hydra(df,y,BrayCurtis,@formula(1~X+Z+W),10000)
+R"ad =adonis2($y ~X+Z+W,$df,10000)"
+n_terms = 3
+
+@test sum(R"ad$Df" .== Hyde.results[:,"Df"]) == n_terms +2
+@test sum(R"ad$SumOfSqs".≈ Hyde.results[:,"SumOfSqs"]) == n_terms +2
+@test sum(R"ad$R2".≈ Hyde.results[:,"R2"]) == n_terms +2
+@test sum(R"ad$F".≈ Hyde.results[:,"F"]) == n_terms
